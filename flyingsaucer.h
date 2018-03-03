d2f1ace40c0d83c60a292d1af482f6e93fca8b82
Sean C <uclaemail5895@gmail.com>
2018-03-02 23:03:32 -0800

    Adds plans to replace the spawner class by having the flying saucer manage the spawning and despawning of human objects.

--- flyingsaucer.h
+++ flyingsaucer.h
@@ -3,6 +3,7 @@
 
 #include <QGraphicsRectItem>
 #include <QKeyEvent>
+#include <vector>
 
 
 class Human;
@@ -12,6 +13,7 @@ class FlyingSaucer: public QGraphicsRectItem
 public:
     FlyingSaucer();
     void keyPressEvent(QKeyEvent* input);
+    std::vector<Human*> population; // make this the live updater
 };
 
 #endif // FLYINGSAUCER_H
